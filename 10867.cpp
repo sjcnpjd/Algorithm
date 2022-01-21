618a96d52524acf052e3ffc2ad67f5c06234b944
sjcnpjd <sjcnpjd960@gmail.com>
2022-01-21 21:50:24 +0900

    중복빼고정렬하기

---
+++ 10867.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	int temp=0;
+	cin >> n;
+
+	vector<int>v(n);	//vector 선언
+
+	for (int i = 0; i < n; i++)	//n개의 정수입력을 하는 반복문
+	{
+		cin >> v[i];
+	}
+	
+	sort(v.begin(), v.end());	//sort함수를 사용하여 오름차순 정렬
+
+	for (int i = 0; i < n; i++)	//출력
+	{
+		if (temp == v[i])	//중복을 빼기 위한 조건문
+			continue;
+		cout << v[i] << " ";
+		temp = v[i];
+	}
+
+	return 0;
+}
