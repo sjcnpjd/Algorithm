29c3ddc1cfceffdb106f1ba60d861f4cf5c1b972
sjcnpjd <sjcnpjd960@gmail.com>
2022-01-27 13:18:11 +0900

    K번째 수

---
+++ 11004.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	
+	cin >> n >> k;
+	vector<int>v(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());	//오름차순정렬
+
+	cout << v[k - 1];	//해당인덱스의 값 출력
+	
+
+	return 0;
+}
