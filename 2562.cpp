a51a05717a049d3a9130358b8e7efbede005d54d
sjcnpjd <sjcnpjd960@gmail.com>
2022-01-28 23:52:14 +0900

    최댓값

---
+++ 2562.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	vector<pair<int,int>>v(9);
+
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> v[i].first;
+		v[i].second = i+1;
+	}
+
+
+	sort(v.begin(), v.end());
+
+	cout << v[8].first << '\n';
+	cout << v[8].second << '\n';
+
+
+
+	return 0;
+}
