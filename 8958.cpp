ce240166f1a5be9d2667e456c5bc76697ff9f3c9
sjcnpjd <sjcnpjd960@gmail.com>
2022-03-08 02:24:43 +0900

    OX퀴즈

---
+++ 8958.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    
+    int n;
+    string temp;
+    int sum = 0;
+    int num = 0;
+    vector<int>v;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {   
+        sum = 0;
+        num = 0;
+        cin >> temp;
+
+        for (int j = 0; j < temp.length(); j++)
+        {
+            if (temp[j] == 'O')
+            {
+                num++;
+            }
+            else
+            {
+                num = 0;
+            }
+            sum += num;
+        }
+        v.push_back(sum);
+    }
+
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << '\n';
+
+    return 0;
+}
