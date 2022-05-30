ad5108472b37ab7bf2fdcf822d8d3233d3b43adf
sjcnpjd <98098127+sjcnpjd@users.noreply.github.com>
2022-05-30 14:34:33 +0900

    행복유치원

---
+++ 13164.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+int n, k;
+vector<int>v;
+vector<int>result;
+int temp;
+int answer = 0;
+
+int main()
+{	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+	
+	result.resize(n-1);
+
+	for (int i = 0; i < n-1; i++)
+	{
+		result[i] = v[i + 1] - v[i];
+	}
+
+	sort(result.begin(), result.end());
+
+	for (int i = 0; i < n - k; i++)
+	{
+		answer += result[i];
+	}
+
+	cout << answer;
+	return 0;
+}
